d98fe3ff88a8c2b025f0841e7cd68decb0e3025f
luisanch <luissanchezmtn@gmail.com>
2022-09-20 21:36:42 +0200

    Data Stream Parser Example
    Example for UDP data stream parser has been added

--- dev/DataModel/ExampleUPD.py
+++ dev/DataModel/ExampleUPD.py
@@ -1,44 +1,98 @@
-from asyncio.windows_events import NULL
+from asyncio.windows_events import NULL 
 import socket
 import time
+import pynmea2
+from pyais import decode as ais_decode
 
+eol_separator = '\r\n'
+bad_eol_separator = '\\r\\n'
+loop_limit = 1
+
+# Variables for logging the UDP stream
+log_file_name = "datstream_5min.txt"
 seconds = 300
-timeout = time.time() + seconds   
+timeout = time.time() + seconds 
+log_stream = False  
+
+#variables for identifying messages 
+parsed_msg_tags = []
+unknown_msg_tags = []
+
+#variables for console output   
+tag_verbose = False
+parsed_message_verbose = False
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("fagitrelay.it.ntnu.no",25508))
 
-msg = NULL
-sensor_data = {}
-eol_string = '\r\n'
-msg_types = {'!': 'type1_',
-            '$': 'type2_' }
+def save_individual_tags(raw_msg, target_list, verbose = False):
+    #Tiny  
+    tag = 'unknown'
+    decoded_msg = raw_msg.decode(encoding='ascii')
 
-def parse_message(msg, msg_ref, eol_string): 
-    msg_data = msg.strip(eol_string).split(',')
-    msg_id = msg_data[0]
-    #print(msg, msg_data[0])
-    print(msg)
-    for ref_key in msg_ref:
-        msg_id = msg_id.replace(ref_key, msg_ref[ref_key])
+    if decoded_msg[0] == '$' or decoded_msg[0] == '!':
+        tag = decoded_msg.split(',')[0]
+    else:
+        tag = "encoded_byte_array"
+    
+    if target_list.count(tag) == 0:
+        target_list.append(tag)
+    
+    if verbose:
+        print('tag {} for list {} has been added for message: \r\n {}'.format(tag , repr(target_list), raw_msg))
 
-    msg_data.pop(0)
-    return msg_id, msg_data
+def update_data_object(parsed_msg, what, verbose = False):
+    #update this method in order to save incoming data into a Singleton
+    #also create singleton
+    if verbose:
+        print('type {}{} Message: {}'.format(type(parsed_msg), what, repr(parsed_msg)))
+    return
 
-def update_sensor_data(sensor_data, incoming_message):
-    sensor_data[parsed_message[0]] = parsed_message[1]
+def split_collated_string(raw_msg, eol_separator, bad_eol_separator):
+    parsed_string = raw_msg.decode(encoding='ascii') 
 
-f = open("datstream_5min.txt", "a")
+    # There's probably a better way to handle the bad double backslash
+    parsed_string = parsed_string.replace(bad_eol_separator, eol_separator)
+    string_list = parsed_string.strip().split(eol_separator)  
+    return string_list
+
+def parse_nmea_message(raw_msg, loop_limit = 1, __loop_count = 0):    
+    try:  
+        decoded_msg = raw_msg.decode(encoding='ascii')
+        parsed_msg = pynmea2.parse(decoded_msg)  
+        update_data_object(parsed_msg, 'NMEA', parsed_message_verbose)
+        save_individual_tags(raw_msg, parsed_msg_tags, tag_verbose)
+    except pynmea2.ParseError:
+        try:  
+            parsed_msg = ais_decode(raw_msg)
+            update_data_object(parsed_msg, 'AIS', parsed_message_verbose)
+            save_individual_tags(raw_msg, parsed_msg_tags, tag_verbose)
+        except:
+            try:
+                assert(__loop_count < loop_limit)
+                __loop_count += 1
+                string_list = split_collated_string(raw_msg, eol_separator, bad_eol_separator)
+                for entry in string_list:
+                    entry = entry + eol_separator 
+                    entry_bin = entry.encode(encoding="ascii")
+                    parse_nmea_message(entry_bin, loop_limit, __loop_count)
+            except:
+                try: 
+                    save_individual_tags(raw_msg, unknown_msg_tags, tag_verbose)
+                    print('Unable to parse message: {}'.format(raw_msg))  
+                except:
+                    print('Unable to parse or save message tag: {}'.format(raw_msg))
+    
+if log_stream:
+    f = open(log_file_name, "a")
 
 while True:
+    raw_msg = s.recv(1024)  
+    parse_nmea_message(raw_msg, loop_limit) 
 
+    if log_stream:
+        f.write(raw_msg)
 
-    msg = s.recv(1024).decode()
-    f.write(msg)
-    parsed_message = parse_message(msg, msg_types, eol_string)
-    update_sensor_data(sensor_data, parsed_message)
-
-    if time.time() > timeout:
+    if time.time() > timeout and log_stream:
         f.close
         break
-    #print(sensor_data) 
