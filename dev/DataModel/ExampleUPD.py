f87a54aab5176ecf70edcc05582280111bf093e9
luisanch <luissanchezmtn@gmail.com>
2022-09-22 15:31:44 +0200

    style update

--- dev/DataModel/ExampleUPD.py
+++ dev/DataModel/ExampleUPD.py
@@ -24,24 +24,21 @@ parsed_msg_tags = []
 unknown_msg_tags = []
 
 #variables for console output   
-tag_verbose = False
-unparsed_tag_verbose = False
+tag_verbose = True
+unparsed_tag_verbose = True
 parsed_message_verbose = False
-parse_error_verbose = True
+parse_error_verbose = False
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("fagitrelay.it.ntnu.no",25508))
 
 def save_individual_tags(raw_msg, target_list, target_list_name, parsed_message = NULL, verbose = False):
-    tag = 'unknown'
+    tag = "encoded_byte_array"
     decoded_msg = raw_msg.decode(encoding='ascii')
-    decoded_msg = decoded_msg.replace(eol_separator, '')
-
-    if decoded_msg[0] == '$' or decoded_msg[0] == '!':
-        tag = decoded_msg.split(',')[0]
-    else:
-        tag = "encoded_byte_array"
-    
+    decoded_msg = decoded_msg.replace(eol_separator, '') 
+    for begin_identifier in msg_begin_identifiers:
+        if decoded_msg[0] == begin_identifier:
+            tag = decoded_msg.split(',')[0] 
     if target_list.count(tag) == 0:
         target_list.append(tag)
         if verbose:
@@ -65,10 +62,8 @@ def replace_bad_eol(raw_msg, eol_separator, bad_eol_separators):
     for separator in bad_eol_separators:
         bad_separator = separator[0]
         good_separator = separator[1]
-
         while parsed_string.find(bad_separator) != -1: 
             parsed_string = parsed_string.replace(bad_separator, good_separator)
-
     string_list = parsed_string.strip().split(eol_separator)   
     return string_list
 
@@ -85,17 +80,13 @@ def parse_ais(raw_msg):
 
 def parse_bad_eol(raw_msg, loop_limit, __loop_count):
     assert(__loop_count < loop_limit) 
-
     __loop_count += 1
     string_list = replace_bad_eol(raw_msg, eol_separator, bad_eol_separators) 
-
     assert(len(string_list) > 1)
-
     for entry in string_list:
         entry = entry + eol_separator 
         entry_bin = entry.encode(encoding="ascii")
         parse_message(raw_msg = entry_bin, loop_limit = loop_limit, __loop_count = __loop_count)
-
 
 def parse_message(raw_msg, loop_limit = 1, __loop_count = 0):    
     try: parse_nmea(raw_msg)
@@ -118,10 +109,8 @@ if log_stream:
 while True:
     raw_msg = s.recv(4096)  
     parse_message(raw_msg, loop_limit) 
-
     if log_stream:
         f.write(raw_msg)
-
     if time.time() > timeout and log_stream:
         f.close
         break
