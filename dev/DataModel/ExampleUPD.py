0f89fa862b848495e01c7daa68b2e9c5572c457d
luisanch <luissanchezmtn@gmail.com>
2022-09-22 10:58:13 +0200

    verbosity updates

--- dev/DataModel/ExampleUPD.py
+++ dev/DataModel/ExampleUPD.py
@@ -20,15 +20,18 @@ unknown_msg_tags = []
 
 #variables for console output   
 tag_verbose = False
+unparsed_tag_verbose = True
 parsed_message_verbose = False
+parse_error_verbose = False
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("fagitrelay.it.ntnu.no",25508))
 
-def save_individual_tags(raw_msg, target_list, verbose = False):
+def save_individual_tags(raw_msg, target_list, parsed_message = NULL, verbose = False):
     #Tiny  
     tag = 'unknown'
     decoded_msg = raw_msg.decode(encoding='ascii')
+    decoded_msg = decoded_msg.replace(eol_separator, '')
 
     if decoded_msg[0] == '$' or decoded_msg[0] == '!':
         tag = decoded_msg.split(',')[0]
@@ -37,9 +40,10 @@ def save_individual_tags(raw_msg, target_list, verbose = False):
     
     if target_list.count(tag) == 0:
         target_list.append(tag)
-    
-    if verbose:
-        print('tag {} for list {} has been added for message: \r\n {}'.format(tag , repr(target_list), raw_msg))
+        if verbose:
+            print('tag {} for list {} has been added for message: \r\n {}'.format(tag , repr(target_list), raw_msg))
+            if parsed_message is not NULL:
+                print('saved parsed message is: {}'.format(repr(parsed_message)))
 
 def update_data_object(parsed_msg, what, verbose = False):
     #update this method in order to save incoming data into a Singleton
@@ -56,17 +60,17 @@ def split_collated_string(raw_msg, eol_separator, bad_eol_separator):
     string_list = parsed_string.strip().split(eol_separator)  
     return string_list
 
-def parse_nmea_message(raw_msg, loop_limit = 1, __loop_count = 0):    
+def parse_nmea_message(raw_msg, loop_limit = 1,__loop_count = 0):    
     try:  
         decoded_msg = raw_msg.decode(encoding='ascii')
         parsed_msg = pynmea2.parse(decoded_msg)  
         update_data_object(parsed_msg, 'NMEA', parsed_message_verbose)
-        save_individual_tags(raw_msg, parsed_msg_tags, tag_verbose)
+        save_individual_tags(raw_msg, parsed_msg_tags, parsed_msg, tag_verbose)
     except pynmea2.ParseError:
         try:  
             parsed_msg = ais_decode(raw_msg)
             update_data_object(parsed_msg, 'AIS', parsed_message_verbose)
-            save_individual_tags(raw_msg, parsed_msg_tags, tag_verbose)
+            save_individual_tags(raw_msg, parsed_msg_tags, parsed_msg, tag_verbose)
         except:
             try:
                 assert(__loop_count < loop_limit)
@@ -78,10 +82,12 @@ def parse_nmea_message(raw_msg, loop_limit = 1, __loop_count = 0):
                     parse_nmea_message(entry_bin, loop_limit, __loop_count)
             except:
                 try: 
-                    save_individual_tags(raw_msg, unknown_msg_tags, tag_verbose)
-                    print('Unable to parse message: {}'.format(raw_msg))  
+                    save_individual_tags(raw_msg, unknown_msg_tags, verbose = unparsed_tag_verbose)
+                    if parse_error_verbose:
+                        print('Unable to parse message: {}'.format(raw_msg))  
                 except:
-                    print('Unable to parse or save message tag: {}'.format(raw_msg))
+                    if parse_error_verbose:
+                        print('Unable to parse or save message tag: {}'.format(raw_msg))
     
 if log_stream:
     f = open(log_file_name, "a")
