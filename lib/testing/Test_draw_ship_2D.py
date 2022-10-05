556ee54b9cbfd6e19503b95d3d0ee6d90c16f031
Roger Skjetne <roger.skjetne@ntnu.no>
2022-10-05 20:47:33 +0200

    Update Test_draw_ship_2D.py

--- lib/testing/Test_draw_ship_2D.py
+++ lib/testing/Test_draw_ship_2D.py
@@ -32,7 +32,7 @@ psi = np.array([-45, 22.5])*np.pi/180 # Headings of ship
 eta = np.array([[1,2],[2,3],psi])
 
 fig1, ax1 = plt.subplots()  # a figure with a single Axes
-parP = {'ax': ax1, 'Loa': [1.0, 1.9], 'type': 'arrow'}
+parP = {'ax': ax1, 'Loa': [1.0, 1.9], 'type': 'ship'}
 draw_ship_2D(eta,parP)
 
 ax1.set_aspect('equal', 'box')
