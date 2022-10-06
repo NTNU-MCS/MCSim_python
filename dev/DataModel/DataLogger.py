f13af1b513fb2df0a57007a10c365f52a9fbaa07
luisanch <luissanchezmtn@gmail.com>
2022-10-06 21:11:58 +0200

    DataLogger updated
    datalogger get atr method added

--- dev/DataModel/DataLogger.py
+++ dev/DataModel/DataLogger.py
@@ -1,18 +1,37 @@
 class DataLogger:
     def __init__(self, stream_parser):
-        self.__stream_parser = stream_parser
+        self._stream_parser = stream_parser
         self.sorted_data = {}
-        self.__running = False
-        self.__buffer_data = stream_parser.parsed_msg_list
+        self._running = False
+        self._buffer_data = stream_parser.parsed_msg_list
+
+    def _get_nmea_attributes(self, nmea_object):
+        t = type(nmea_object)
+        msg_values = []        
+        msg_atr = []
+        unkown_msg_data = []
+
+        for i , v in enumerate(nmea_object.data): 
+            if i >= len(t.fields):
+                unkown_msg_data.append(v)
+                continue
+            name = t.fields[i][1]
+            msg_atr.append(name)
+            msg_values.append(getattr(nmea_object, name))
+
+        return(msg_atr, msg_values, unkown_msg_data)
     
-    def log_buffered_message(self):
-        if len(self.__buffer_data) < 1: return
-        
-        print(self.__buffer_data[-1])
-        self.__stream_parser.pop_parsed_msg_list()
+    def _log_buffered_message(self):
+        if len(self._buffer_data) < 1: return 
+        nmea_message = self._buffer_data[-1][1]
+        # ToDo: update id when new data is available from stream
+        msg_id = self._buffer_data[-1][0]
+        msg_atr, msg_values, unkown_msg_data = self._get_nmea_attributes(nmea_message) 
+        #print(msg_id, msg_atr, msg_values, unkown_msg_data)
+        self._stream_parser.pop_parsed_msg_list()
 
     def sort_buffered_data(self):
-        self.__running = True
+        self._running = True
 
-        while self.__running:
-            self.log_buffered_message()
+        while self._running: #should make this consistent with other classes
+            self._log_buffered_message()
