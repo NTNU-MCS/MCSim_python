780639a1871d0d014cc53444a8ea64927eeaa904
luisanch <luissanchezmtn@gmail.com>
2022-12-06 01:19:13 +0100

    Added Unity Assets
    Scripts for Gemini Rt simulation

---
+++ dev/DataModel/Unity_Assets/ScenarioRT.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gemini.EMRS.Core;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+
+namespace Gemini.EMRS.ScenarioGenerator {
+    public class ScenarioRT: MonoBehaviour
+    {
+        public GameObject BoatPrefab;
+        private BoatScenarioRT _boatScenario;
+
+        private double sim_time = -1d; 
+
+        void Start()
+        {
+            SetupBoat(); 
+        }
+        
+        void FixedUpdate() { 
+                _boatScenario.UpdateVessel();
+                sim_time = _boatScenario.time;
+        }
+
+    void OnDisable()
+    {   
+        _boatScenario.running = false;
+        
+    }
+
+        private void SetupBoat()
+        {
+            BoatPrefab = Instantiate(BoatPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            _boatScenario = new BoatScenarioRT(BoatPrefab);
+        }
+    }
+}
