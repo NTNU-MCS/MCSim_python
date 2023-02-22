372cd4027c0a56d3f026d579fa7eedfefc6e41f2
luisanch <luissanchezmtn@gmail.com>
2023-02-23 00:18:40 +0100

    unity AIS data

--- dev/DataModel/SimulationServer.py
+++ dev/DataModel/SimulationServer.py
@@ -20,6 +20,7 @@ class SimulationServer:
         self.distance_filter = distance_filter
         self.gunnerus_lat=None
         self.gunnerus_lon=None
+        self._unity_filter = 0.1
         
         if ws_enable:
             websocket.enableTrace(True)
@@ -31,6 +32,11 @@ class SimulationServer:
             self.buffer_size = buffer_size
             self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    def _has_data(self, msg):
+        msg_keys = msg.keys() 
+        has_data = ("lat" in msg_keys and "lon" in msg_keys)
+        return has_data
+
     def stop(self):
         self._running = False
         print('Simulation Client stopped')
@@ -38,31 +44,26 @@ class SimulationServer:
     def dist(self, p,q):
         return math.sqrt( (q[0] - p[0])**2 + (q[1] - p[1])**2 )
 
-    def _validate_coords(self, msg):
+    def _validate_coords(self, msg, distance):
         if (self.gunnerus_lat is None or self.gunnerus_lon is None): 
             return True
         
-        if (msg["message_id"].find("!AI") == 0):
-            msg_keys = msg.keys() 
-            has_data = ("lat" in msg_keys and "lon" in msg_keys)
-
-            if (has_data): 
+        if (msg["message_id"].find("!AI") == 0): 
+            if (self._has_data(msg)): 
                 lat = msg["lat"]  
                 lon = msg["lon"]
                 
                 p = [lat,lon]
                 q = [self.gunnerus_lat,self.gunnerus_lon]
 
-                return self.dist(p,q) < self.distance_filter
+                return self.dist(p,q) < distance
             
         return True 
 
     def _set_pos_history(self,msg):
-        if (msg["message_id"].find("!AI") == 0):
-            msg_keys = msg.keys() 
-            has_data = ("lat" in msg_keys and "lon" in msg_keys)
+        if (msg["message_id"].find("!AI") == 0): 
 
-            if (has_data):
+            if (self._has_data(msg)):
                 message_id = msg["message_id"]
                 lat = msg["lat"]  
                 lon = msg["lon"]                     
@@ -88,22 +89,40 @@ class SimulationServer:
     def _compose_transformed_msg(self, msg):
         northings = self.transform.deg_2_dec(msg['lat'], msg['lat_dir'])
         eastings = self.transform.deg_2_dec(msg['lon'], msg['lon_dir'])
-        altitude = msg['altitude']
+        altitude = msg['altitude'] 
         x,y,z = self.transform.get_xyz(northings, eastings, altitude)
         return(json.dumps({"message_id":"coords","x":x, "y":y, "z":z}, default=str))
     
+    def _compose_ais_msg(self, msg): 
+            northings = msg['lat']
+            eastings = msg['lon']
+            altitude = 0
+            mmsi = msg['mmsi']
+            heading = 0
+
+            if ('heading' in msg.keys()):
+                heading = msg['heading']
+
+            x,y,z = self.transform.get_xyz(northings, eastings, altitude)
+            return(json.dumps({"message_id": "ais","x":x, "y":y, "mmsi":mmsi, "heading": heading}, default=str)) 
 
     def _set_gunnerus_coords(self, msg): 
         self.gunnerus_lon = self.transform.deg_2_dec(msg['lon'], msg['lon_dir'])
         self.gunnerus_lat = self.transform.deg_2_dec(msg['lat'], msg['lat_dir'])
 
     def _send(self, message): 
-        if self._validate_coords(message): 
+        if self._validate_coords(message, self.distance_filter): 
             json_msg = self._compose_msg(message)
 
             if self.ws_enable: 
                 self.ws.send(json_msg)
             if self.address is not None:
+                if (message["message_id"].find("!AI") == 0 and 
+                self._has_data(message) and 
+                self._validate_coords(message, self._unity_filter)):
+                    msg = self._compose_ais_msg(message)
+                    msg = msg.encode('ascii')
+                    self.server.sendto(msg, (self._ip, self._port))
                 if message["message_id"]=="$GPGGA_ext": 
                     self._set_gunnerus_coords(message)
                     msg = self._compose_transformed_msg(message)
@@ -113,7 +132,7 @@ class SimulationServer:
                     msg = json.dumps(message, default=str)
                     msg = msg.encode('ascii')
                     self.server.sendto(msg, (self._ip, self._port))
-
+                
     def pop_buffer(self, index = None):
         if len(self._buffer) < 1: return
 
